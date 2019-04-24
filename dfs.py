00d14b40244cfa09d18efaf6fdc5bf2d963e25e8
wagnerfns <wagnerfelidre@gmail.com>
2019-04-23 23:30:39 -0300

    dfs

--- dfs.py
+++ dfs.py
@@ -1,4 +1,4 @@
-# implementation of an undirected graph using Adjacency Lists
+
 class Vertex:
     def __init__(self, n):
         self.name = n
@@ -26,7 +26,6 @@ class Graph:
 
     def add_edge(self, u, v):
         if u in self.vertices and v in self.vertices:
-            # my YouTube video shows a silly for loop here, but this is a much faster way to do it
             self.vertices[u].add_neighbor(v)
             self.vertices[v].add_neighbor(u)
             return True
@@ -44,6 +43,7 @@ class Graph:
         time += 1
 
         for v in vertex.neighbors:
+       
             if self.vertices[v].color == 'black':
                 self._dfs(self.vertices[v])
         
@@ -57,17 +57,50 @@ class Graph:
         time = 1
         self._dfs(vertex)
 
+
+    def eulerian(self): #check if is eulerian
+        eulerian = 0
+
+
+        for i in range(self.vertex_quantity):
+            count = 0
+            for j in range(self.vertex_quantity):
+                if(self.adjacency[i][j] == 1):
+                    count += 1
+            if(count%2 == 0):
+                eulerian += 1
+        if(self.vertex_quantity == eulerian):
+            print("Euleriano fechado")
+            return(True)
+        elif((self.vertex_quantity - 2) == eulerian):
+            print("Euleriano aberto")
+            return(True)
+        else:
+            print("Nao e euleriano")
+            return(False)
+
+
 g = Graph()
 # print(str(len(g.vertices)))
-a = Vertex('A')
+
+a = Vertex('1')
+b = Vertex('2')
+e = Vertex('5') 
+
+
 g.add_vertex(a)
-g.add_vertex(Vertex('B'))
-for i in range(ord('A'), ord('K')):
+g.add_vertex(b)
+
+g.add_vertex(e)
+
+
+for i in range(ord('1'), ord('9')):
 	g.add_vertex(Vertex(chr(i)))
 
-edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
+
+edges = ['54', '43', '36', '61', '67', '71', '78', '82', '81', '16', '17', '18', '12', '21', '28']
 for edge in edges:
 	g.add_edge(edge[:1], edge[1:])
 
-g.dfs(a)
+g.dfs(e)
 g.print_graph()
