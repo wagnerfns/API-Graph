1ab8ca1c013380bf77120bc83e34395b37daa53a
wagnerfns <wagnerfelidre@gmail.com>
2019-05-01 15:06:34 -0300

    Novas implementações simples
    foi adicionado metodo para verificar se é euleriano, metodo compativel para matriz e lista de adjacencia

--- test_graph.py
+++ test_graph.py
@@ -46,12 +46,9 @@ class Graph:
 			return(False)
 
 	
-	
-	
 	'''
 	First verify if is a object after if is directed and 
 	created a list or matrix, if matrix created a matrix with size vertex
-
 	'''
 	def add_edge(self, vertex1, vertex2):
 		if(isinstance(vertex1, Vertex)) and (isinstance(vertex2, Vertex)): # verify if created vertex
@@ -68,6 +65,36 @@ class Graph:
 				else:
 					self.dictionary_vertex[vertex1.name].add_adjacency(vertex2.name)
 					self.dictionary_vertex[vertex2.name].add_adjacency(vertex1.name)
+	'''
+	First verify if is, matriz or adjacency list, after verify quantity number of vertex even and odd.
+	if the number of vertex is equal to the number of vertex even, is eulerian closed,
+	if the number of vertex less 2 is equal to the number of vertex even, is eulerian open,  
+	if none other, isn't eulerian
+	'''
+	def eulerian(self):
+		eulerian = 0
+		if(self.adjacency_type):
+			for i in range(len(self.dictionary_vertex)):
+				count = 0
+				for j in range(len(self.dictionary_vertex)):
+					if(self.adjacency[i][j] == 1):
+						count += 1
+				if(count%2 == 0):
+					eulerian += 1
+		else:
+			for i in range(len(self.dictionary_vertex)):
+				count = len(self.dictionary_vertex[i].neighbors)
+				if(count%2 == 0):
+					eulerian += 1
+
+		if(len(self.dictionary_vertex) == eulerian):
+			print("Eulerian closed")
+			return(True)
+		elif((len(self.dictionary_vertex) - 2) == eulerian):
+			print("Eulerian open")
+			return(True)
+		else:
+			print("Not is eulerian")
 
 	def print_graph(self, verbose=None):
 		if(verbose):
@@ -82,22 +109,24 @@ class Graph:
 
 #graph = Graph(True, True)  # matriz direcionada
 #graph = Graph(False, True) # matriz nao direcionada
-#graph = Graph(False, False) # list adjacency not directed
-graph = Graph(True, False)  # list adjacency directed
+graph = Graph(False, False) # list adjacency not directed
+#graph = Graph(True, False)  # list adjacency directed
+
 a = Vertex(0)
 b = Vertex(1)
-c = Vertex(2)
-d = Vertex(3)
-e = Vertex(4)
-
+#c = Vertex(2)
+#d = Vertex(3)
+#e = Vertex(4)
+#f = Vertex(5)
 
 graph.add_vertex(a)
 graph.add_vertex(b)
-graph.add_vertex(c)
-graph.add_vertex(d)
-graph.add_vertex(e)
+#graph.add_vertex(c)
+#graph.add_vertex(d)
+#graph.add_vertex(e)
+#graph.add_vertex(f)
 
-#graph.created_matrix()
+graph.created_matrix()
 
 ###
 #print('Matriz criada')
@@ -108,16 +137,25 @@ graph.add_vertex(e)
 ###
 
 ###
+graph.add_edge(a, a)
+graph.add_edge(b, b)
+#graph.add_edge(c, c)
+#graph.add_edge(d, d)
+
 graph.add_edge(a, b)
-graph.add_edge(a, c)
-graph.add_edge(b, c)
-graph.add_edge(c, d)
-graph.add_edge(e, a)
+#graph.add_edge(a, c)
+#graph.add_edge(d, e)
+#graph.add_edge(d, f)
+#graph.add_edge(e, f)
+
+
 ###
 
 ###
 #print('Matriz com as adjacencias')
 #graph.print_graph(True)
-#print("Lista de adjacencia")
-#graph.print_graph(False)
+print("Lista de adjacencia")
+graph.print_graph(False)
 ###
+
+graph.eulerian()
