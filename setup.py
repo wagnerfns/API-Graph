c954ceb400a6309f167c72531fd688d75a9de365
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-02 16:33:08 -0300

    test 5

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='Grafos',
+    version='0.1.1',
+    url='https://github.com/wagnerfns/Grafos',
+    license='MIT License',
+    author='wagnerfns',
+    author_email='wagnerfelidre@gmail.com',
+    keywords='Teste de cobertura grafos',
+    description=u'Teste unitário de grafos',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='test',
+)
