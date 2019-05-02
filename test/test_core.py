c4af1b7a29a1b14dbbd7b246910a8f93676cf19f
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-02 09:07:44 -0300

    Novos testes
    Foi criado novos teste para matrizes eulerianas e não eulerianas

--- test/test_core.py
+++ test/test_core.py
@@ -1,6 +1,6 @@
 import unittest
-from test_graph import Graph
-from test_graph import Vertex
+from graph import Graph
+from graph import Vertex
 
 
 class TestMethods(unittest.TestCase):
@@ -46,9 +46,59 @@ class TestMethods(unittest.TestCase):
 		graph.add_edge(a, a)
 		graph.add_edge(b, b)
 		self.assertEqual(graph.add_edge(a, b), True, msg='Teste 4')
+	#def test_search_vertex_matrix(self):
+	#def test_search_vertex_list(self):
+	def test_eulerian_matrix_close(self):
+		graph = Graph(True, False)
+		a = Vertex(0)
+		b = Vertex(1)
+		graph.add_vertex(a)
+		graph.add_vertex(b)
+		graph.created_matrix()
+		graph.add_edge(a, a)
+		graph.add_edge(b, b)
+		graph.add_edge(a, b)
+		self.assertEqual(graph.eulerian(), "Eulerian closed", msg='Teste 5')
 
+	def test_eulerian_matrix_open(self):
+		graph = Graph(True, False)
+		a = Vertex(0)
+		b = Vertex(1)
+		c = Vertex(2)
+		d = Vertex(3)
+		e = Vertex(4)
+		f = Vertex(5)
+		graph.add_vertex(a)
+		graph.add_vertex(b)
+		graph.add_vertex(c)
+		graph.add_vertex(d)
+		graph.add_vertex(e)
+		graph.add_vertex(f)
+		graph.created_matrix()
+		graph.add_edge(a, a)
+		graph.add_edge(b, b)
+		graph.add_edge(c, c)
+		graph.add_edge(d, d)
+		graph.add_edge(a, b)
+		graph.add_edge(a, c)
+		graph.add_edge(d, e)
+		graph.add_edge(d, f)
+		graph.add_edge(e, f)
+		self.assertEqual(graph.eulerian(), "Eulerian open", msg='Teste 6')
 
-'''
-def test_eulerian_true_matrix_close(self):
-def test_eulerian_true_matrix_open(self):
-def test_eulerian_false_matrix(self):'''
+	def test_not_eulerian_matrix(self):
+		graph = Graph(True, False)
+		a = Vertex(0)
+		b = Vertex(1)
+		graph.add_vertex(a)
+		graph.add_vertex(b)
+
+		graph.created_matrix()
+
+		graph.add_edge(a, a)
+		graph.add_edge(a, b)
+
+		self.assertEqual(graph.eulerian(), "Not is eulerian", msg='Teste 7')
+
+if __name__ == '__main__':
+    unittest.main()
