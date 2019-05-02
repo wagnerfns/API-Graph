1e2b9ebfd783f1d848229df40628964d86c7bbc4
wagnerfns <wagnerfelidre@gmail.com>
2019-05-01 23:07:10 -0300

    Novo projeto para graph
    Novo projeto com uma classe para aresta e uma classe para grafo

--- graph.py
+++ graph.py
@@ -1,100 +1,189 @@
-class Graph():
-    
-    '''
-    The graph per default is not directed and is matrix.
+class Vertex:
+	def __init__(self, n):
+		self.name = n
+		self.neighbors = list() #vertices ligados a esse vertice
+		self.discovery = 0
+		self.finish = 0
+		self.color = 'white'
+	
+	def add_adjacency(self, v):
+		if v not in self.neighbors:
+			self.neighbors.append(v) #adiciona um vizinho ao vertice
+			self.neighbors.sort() #ordena
+	
+	#def search_vertex(self, index):
+	#	print(f"Vertex: {index} - {self.dictionary_vertex}")
 
-    other settings:
+class Graph:
+	def __init__(self, verbose=None, directed=None):
+		self.adjacency = [] # cria lista para adjacencia
+		self.directed = directed #flag para saber se eh direcioando ou nao
+		self.adjacency_type = verbose # flag para saber eh lista ou matriz
+		self.dictionary_vertex = {}
+		
+		
+	time = 0
+	
+	'''
+	If choice is list created a list or a matrix
+	'''
+	def created_matrix(self):
+		vertex_quantity = len(self.dictionary_vertex)
+		for i in range(vertex_quantity):
+			list_v = []
+			for i in range(vertex_quantity):
 
-    1 - directed: True and verbose: True, is a matrix directed
+				list_v.append(0)
+			self.adjacency.append(list_v)
+				
+	def add_vertex(self, vertex):
+		#verifica se eh um vertice e se ja nao foi um  vertice inserido
+		if isinstance(vertex, Vertex) and vertex.name not in self.dictionary_vertex:
+			self.dictionary_vertex[vertex.name] = vertex
 
-    2 - directed: False and verbose: False, is a list directed
+			return(True)
+		else: # return false if vertex insured
+			return(False)
 
-    3 - directed: True and verbose: False, is a list not directed
+	
+	'''
+	First verify if is a object after if is directed and 
+	created a list or matrix, if matrix created a matrix with size vertex
+	'''
+	def add_edge(self, vertex1, vertex2):
+		if(isinstance(vertex1, Vertex)) and (isinstance(vertex2, Vertex)): # verify if created vertex
+			if(self.adjacency_type):# created matrix
+								
+				if(self.directed): #if directed or not directed
+					self.adjacency[vertex1.name][vertex2.name] = 1
+				else:
+					self.adjacency[vertex1.name][vertex2.name] = 1
+					self.adjacency[vertex2.name][vertex1.name] = 1
+				return(True)
+			else: # created list
+				if(self.directed): #if directed or not directed
+					self.dictionary_vertex[vertex1.name].add_adjacency(vertex2.name)
+				else:
+					self.dictionary_vertex[vertex1.name].add_adjacency(vertex2.name)
+					self.dictionary_vertex[vertex2.name].add_adjacency(vertex1.name)
+				return(True)
 
-    '''
-    def __init__(self, vertex_quantity, directed=False, verbose=True):
-        self.adjacency = [] # created a list for adjacency
-        self.vertex_quantity = vertex_quantity
-        self.directed = directed    
-        self.adjacency_type = verbose
-        
-        if(self.adjacency_type): # if verbose false create a list, if true creat a matrix
-            for i in range(vertex_quantity):
-                list_v = []
-                for i in range(vertex_quantity):
-                    list_v.append(0)
-                self.adjacency.append(list_v)
-        else: #create list
-            for i in range(vertex_quantity):
-                self.adjacency.append([])
+	'''
+	First verify if is, matriz or adjacency list, after verify quantity number of vertex even and odd.
+	if the number of vertex is equal to the number of vertex even, is eulerian closed,
+	if the number of vertex less 2 is equal to the number of vertex even, is eulerian open,  
+	if none other, isn't eulerian
+	'''
+	def eulerian(self):
+		eulerian = 0
+		if(self.adjacency_type):
+			for i in range(len(self.dictionary_vertex)):
+				count = 0
+				for j in range(len(self.dictionary_vertex)):
+					if(self.adjacency[i][j] == 1):
+						count += 1
+				if(count%2 == 0):
+					eulerian += 1
+		else:
+			for i in range(len(self.dictionary_vertex)):
+				count = len(self.dictionary_vertex[i].neighbors)
+				if(count%2 == 0):
+					eulerian += 1
 
-    def add_vertex(self, vertex1, vertex2, directed=False ,verbose=True):
-        
-        if(self.directed == directed == True): #check if is directed
-            
-            if(self.adjacency_type == verbose == True): #check if is matrix
-                self.adjacency[vertex1][vertex2] = 1
-
-            elif(self.adjacency_type == verbose == False): #check if is list
-                self.adjacency[vertex1].append(vertex2) 
-        else:
-            if(self.adjacency_type == verbose == True): #check if is matrix
-                self.adjacency[vertex1][vertex2] = 1
-                self.adjacency[vertex2][vertex1] = 1
-
-            elif(self.adjacency_type == verbose == False): #check if is list
-                self.adjacency[vertex1].append(vertex2)
-                self.adjacency[vertex2].append(vertex1)
-
-        return(self.adjacency)
-        
-    def search_vertex(self, index):
-        print(f"vertex: {index} - {self.adjacency[index]}")    
-        return(self.adjacency[index])
-
-    def print_graph(self, verbose=True):
-        if(self.adjacency_type == verbose == True): #matrix   
-            
-            index = []
-            for i in range(self.vertex_quantity): #print index matrix
-                index.append(i)
-            print('\t',index)     
-
-            for i in range(self.vertex_quantity): #print matriz for line
-                print(f'{[i]}\t {self.adjacency[i]}')
-        else: #list
-            for i in range(self.vertex_quantity):
-                print(f"[{i}] -> {self.adjacency[i]}")
-
-    def eulerian(self): #check if is eulerian
-        eulerian = 0
-        for i in range(self.vertex_quantity):
-            count = 0
-            for j in range(self.vertex_quantity):
-                if(self.adjacency[i][j] == 1):
-                    count += 1
-            if(count%2 == 0):
-                eulerian += 1
-        if(self.vertex_quantity == eulerian):
-            print("Euleriano fechado")
-            return(True)
-        elif((self.vertex_quantity - 2) == eulerian):
-            print("Euleriano aberto")
-            return(True)
-        else:
-            print("Nao e euleriano")
-            return(False)
-
-
-graph = Graph(2, True, True)
-t = graph.add_vertex(0, 1, True, True)
-print(t)
-#graph.add_vertex(1,0)
+		if(len(self.dictionary_vertex) == eulerian):
+			print("Eulerian closed")
+			return(True)
+		elif((len(self.dictionary_vertex) - 2) == eulerian):
+			print("Eulerian open")
+			return(True)
+		else:
+			print("Not is eulerian")
 
 
+	def _dfs(self, vertex):
+		global time
+		vertex.color = 'gray'
+		vertex.discovery = time
+		time += 1
+
+		for i in vertex.neighbors:
+			if(self.dictionary_vertex[i].color == 'white'):
+				self._dfs(self.dictionary_vertex[i])
+
+		vertex.color = 'black'
+		vertex.finish = time
+		time += 1
+
+	def dfs(self, vertex):
+		global time
+
+		time = 1
+		self._dfs(vertex)
+
+	'''
+	If print graph equal (True), print a matrix, if graph equal (False) print list adjacency
+	if print way graph dfs equal (False and True) 
+	'''
+	def print_graph(self, verbose=None, dfs = False):
+		if(verbose):
+			for i in range(len(self.dictionary_vertex)):
+				print(f'{[i]} -> {self.adjacency[i]}')
+		else:
+			if not dfs:
+				for key in sorted(list(self.dictionary_vertex.keys())):
+					print(f"{key} - {self.dictionary_vertex[key].neighbors}")
+			else:
+				for key in sorted(list(self.dictionary_vertex.keys())):
+					print(f"{key} - {self.dictionary_vertex[key].neighbors} - {self.dictionary_vertex[key].discovery}")
 
 
-#graph.eulerian()
+#graph = Graph(True, True)  # matriz direcionada
+#graph = Graph(True, False) # matriz nao direcionada
+graph = Graph(False, False) # list adjacency not directed
+#graph = Graph(False, True)  # list adjacency directed
+'''
+a = Vertex(1)
+b = Vertex(2)
 
-graph.print_graph()
+c = Vertex(3)
+d = Vertex(4)
+e = Vertex(5)
+f = Vertex(6)
+g = Vertex(7)
+h = Vertex(8)
+
+
+
+graph.add_vertex(a)
+graph.add_vertex(b)
+graph.add_vertex(c)
+graph.add_vertex(d)
+graph.add_vertex(e)
+graph.add_vertex(f)
+graph.add_vertex(g)
+graph.add_vertex(h)
+
+
+
+graph.add_edge(a, b)
+graph.add_edge(a, h)
+graph.add_edge(a, f)
+graph.add_edge(a, g)
+graph.add_edge(b, h)
+graph.add_edge(c, f)
+graph.add_edge(d, c)
+graph.add_edge(e, d)
+graph.add_edge(f, g)
+graph.add_edge(g, h)
+
+
+'''
+
+print("Lista de adjacencia")
+#graph.print_graph(False)
+###
+
+#graph.dfs(e)
+
+graph.print_graph(False)
 
