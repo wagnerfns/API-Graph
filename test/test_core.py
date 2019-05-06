cd6c173ab14dd5296143207919529740405c80f8
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-06 12:54:09 -0300

    Novas coberturas
    Foi adicionado cobertura para o dfs e bfs para grafos não direcionados

--- test/test_core.py
+++ test/test_core.py
@@ -48,7 +48,7 @@ class TestMethods(unittest.TestCase):
 		self.assertEqual(graph.add_edge(a, b), True, msg='Teste 4')
 	#def test_search_vertex_matrix(self):
 	#def test_search_vertex_list(self):
-	def test_eulerian_matrix_close(self):
+	def test_eulerian_matrix(self):
 		graph = Graph(True, False)
 		a = Vertex(0)
 		b = Vertex(1)
@@ -58,7 +58,7 @@ class TestMethods(unittest.TestCase):
 		graph.add_edge(a, a)
 		graph.add_edge(b, b)
 		graph.add_edge(a, b)
-		self.assertEqual(graph.eulerian(), "Eulerian closed", msg='Teste 5')
+		self.assertEqual(graph.eulerian(), "Is Eulerian", msg='Teste 5')
 
 	def test_eulerian_matrix_open(self):
 		graph = Graph(True, False)
@@ -99,6 +99,74 @@ class TestMethods(unittest.TestCase):
 		graph.add_edge(a, b)
 
 		self.assertEqual(graph.eulerian(), "Not is eulerian", msg='Teste 7')
+
+	def test_bfs_list_not_directed(self):
+		graph = Graph(False, False) # list adjacency not directed
+
+		a = Vertex(1)
+		b = Vertex(2)
+		c = Vertex(3)
+		d = Vertex(4)
+		e = Vertex(5)
+		f = Vertex(6)
+
+		graph.add_vertex(a)
+		graph.add_vertex(b)
+		graph.add_vertex(c)
+		graph.add_vertex(d)
+		graph.add_vertex(e)
+		graph.add_vertex(f)
+
+		graph.add_edge(a, b)
+		graph.add_edge(a, c)
+		graph.add_edge(b, d)
+		graph.add_edge(c, d)
+		graph.add_edge(c, e)
+		graph.add_edge(d, c)
+		graph.add_edge(d, e)
+		graph.add_edge(d, f)
+		graph.add_edge(e, c)
+		graph.add_edge(e, d)
+		graph.add_edge(e, f)
+		graph.add_edge(f, e)
+		graph.add_edge(f, d)
+
+		self.assertEqual(graph.bfs(a), True, msg='Teste 8')
+
+	def test_dfs_list_not_directed(self):
+		graph = Graph(False, False) # list adjacency not directed
+
+		a = Vertex(1)
+		b = Vertex(2)
+		c = Vertex(3)
+		d = Vertex(4)
+		e = Vertex(5)
+		f = Vertex(6)
+		g = Vertex(7)
+		h = Vertex(8)
+
+		graph.add_vertex(a)
+		graph.add_vertex(b)
+		graph.add_vertex(c)
+		graph.add_vertex(d)
+		graph.add_vertex(e)
+		graph.add_vertex(f)
+		graph.add_vertex(g)
+		graph.add_vertex(h)
+
+		graph.add_edge(a, b)
+		graph.add_edge(a, h)
+		graph.add_edge(a, f)
+		graph.add_edge(a, g)
+		graph.add_edge(b, h)
+		graph.add_edge(c, f)
+		graph.add_edge(d, c)
+		graph.add_edge(e, d)
+		graph.add_edge(f, g)
+		graph.add_edge(g, h)
+
+		self.assertEqual(graph.dfs(e), True, msg='Teste 9')
+		
 
 if __name__ == '__main__':
     unittest.main()
