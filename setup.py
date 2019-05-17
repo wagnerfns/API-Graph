44b3605b41b29621b790fd5f9e4e32244c851238
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-17 10:45:45 -0300

    controle de teste

--- setup.py
+++ setup.py
@@ -2,15 +2,5 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Grafos',
-    version='0.1.1',
-    url='https://github.com/wagnerfns/Grafos',
-    license='GPL License',
-    author='wagnerfns',
-    author_email='',
-    keywords='Teste de cobertura grafos',
-    description=u'Teste unitário de grafos',
-    packages=find_packages(),
-    install_requires=[],
-    test_suite='test',
+    install_requires=['pytest'],
 )
