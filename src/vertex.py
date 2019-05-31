c7109251cd82761b5ba49da6f53d7b3b6a31f655
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-31 14:06:41 -0300

    novo arquivo
    criado um arquivo para classe vertex

---
+++ src/vertex.py
@@ -0,0 +1,20 @@
+"""Test."""
+
+
+class Vertex:
+    """Create Vertex."""
+
+    def __init__(self, n):
+        """Test."""
+        self.name = n
+        self.neighbors = list()  # vertices ligados a esse vertice
+        self.discovery = 0
+        self.finish = 0
+        self.distance = 9999
+        self.color = 'white'
+
+    def add_array(self, v):
+        """Test."""
+        if v not in self.neighbors:
+            self.neighbors.append(v)  # adiciona um vizinho ao vertice
+            self.neighbors.sort()  # ordena
