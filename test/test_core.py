483978619c1bea74f7d3dbc7868842379481e8a4
wagnernegrao <wagnerfelidre@gmail.com>
2019-09-11 18:40:13 -0300

    separate some test

--- test/test_core.py
+++ test/test_core.py
@@ -8,52 +8,6 @@ from scripts.vertex import Vertex
 class TestMethods(unittest.TestCase):
     """Create Test."""
 
-    def test_add_vertex_array_not_directed(self):
-        """Test add vertex array not directed."""
-        graph = Graph(True, False)
-        a = Vertex(0)
-        b = Vertex(1)
-        graph.add_vertex(a)
-        graph.add_vertex(b)
-        graph.create_array()
-        graph.add_edge(a, a)
-        graph.add_edge(b, b)
-        self.assertEqual(graph.add_edge(a, b), True, msg='Teste 1')
-
-    def test_add_vertex_array_directed(self):
-        """Test add vertex array directed."""
-        graph = Graph(True, True)
-        a = Vertex(0)
-        b = Vertex(1)
-        graph.add_vertex(a)
-        graph.add_vertex(b)
-        graph.create_array()
-        graph.add_edge(a, a)
-        graph.add_edge(b, b)
-        self.assertEqual(graph.add_edge(a, b), True, msg='Teste 2')
-
-    def test_add_vertex_list_not_directed(self):
-        """Test add vertex list not directed."""
-        graph = Graph(False, False)
-        a = Vertex(0)
-        b = Vertex(1)
-        graph.add_vertex(a)
-        graph.add_vertex(b)
-        graph.add_edge(a, a)
-        graph.add_edge(b, b)
-        self.assertEqual(graph.add_edge(a, b), True, msg='Teste 3')
-
-    def test_add_vertex_list_directed(self):
-        """Test add vertex list directed."""
-        graph = Graph(False, True)
-        a = Vertex(0)
-        b = Vertex(1)
-        graph.add_vertex(a)
-        graph.add_vertex(b)
-        graph.add_edge(a, a)
-        graph.add_edge(b, b)
-        self.assertEqual(graph.add_edge(a, b), True, msg='Teste 4')
-
     def test_search_vertex_array_not_directed(self):
         """Test search vertex array not directed."""
         graph = Graph(True, False)
