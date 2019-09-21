9a160aa3b259805aebb26cd664db309822152b9a
wagnernegrao <wagnerfelidre@gmail.com>
2019-09-21 13:48:41 -0300

    rename elements

--- scripts_2.0/graph.py
+++ scripts_2.0/graph.py
@@ -6,10 +6,10 @@ class Graph(Graphy):
     def __init__(self, strategy):
         super().__init__()
         self._strategy = strategy
-        self.directed = None #self._strategy.directed()
-        self.adj_type = None
+        self.directed = None
+        self.graph_type = None
 
-    def graph_type(self):
+    def initialize_graph(self):
 
         self.directed = self._strategy.directed()
-        self.adj_type = self._strategy.adj_type()
+        self.graph_type = self._strategy.graph_type()
