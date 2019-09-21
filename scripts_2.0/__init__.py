fd92e33701a549db87bd9882a2a1e7c38aa70fea
wagnernegrao <wagnerfelidre@gmail.com>
2019-09-21 13:24:55 -0300

    test implemented

---
+++ scripts_2.0/__init__.py
@@ -0,0 +1,14 @@
+from graphy import Graphy
+from graph_directed import GraphDirected
+from graph_o import Graph
+
+
+graph = GraphDirected()
+
+c_graph = Graph(graph)
+c_graph.graph_type()
+
+
+print(c_graph.adj_type)
+print(c_graph.directed)
+print("True")
