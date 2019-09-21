ab7fd5b31616d3ab8b61f126c6e024830d06501d
wagnernegrao <wagnerfelidre@gmail.com>
2019-09-21 14:35:51 -0300

    more implements

--- scripts_2.0/__init__.py
+++ scripts_2.0/__init__.py
@@ -1,14 +1,28 @@
-from graphy import Graphy
-from graph_directed import GraphDirected
+from array_directed import ArrayDirected
+from array_not_directed import ArrayNotDirected
+
 from graph import Graph
 
 
-graph = GraphDirected()
+graph1 = ArrayDirected()
 
-c_graph = Graph(graph)
-c_graph.graph_type()
+c_graph = Graph(graph1)
+c_graph.initialize_graph()
 
 
-print(c_graph.adj_type)
+
 print(c_graph.directed)
-print("True")
+print(c_graph.graph_type)
+print("--/-/-/-/-/-/")
+
+
+
+graph2 = ArrayNotDirected()
+
+c_graph2 = Graph(graph2)
+c_graph2.initialize_graph()
+
+
+print(c_graph2.directed)
+print(c_graph2.graph_type)
+print("--/-/-/-/-/-/")
